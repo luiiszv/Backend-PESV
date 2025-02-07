a3b681de7b505b3e537540cb2511b75bc6ce4528
luiiszv <martinesj405@gmail.com>
2025-02-07 16:04:16 -0500

    feat: vehiculos

--- src/PESV/controllers/vehiculos.controller.js
+++ src/PESV/controllers/vehiculos.controller.js
@@ -5,14 +5,12 @@ export const getAllVehiculos = async (req, res) => {
 
         const response = await findAllVehiculos();
         return res.status(200).json(response);
-
     } catch (error) {
         console.log(error);
         res.status(400).json({
             message: "Something went wrong in getAllVehiculos",
             error,
         });
-
     }
 
 }
