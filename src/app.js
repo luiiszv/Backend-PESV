9f26931c3492c240cb858e99bad11e7b6a6c154c
luiiszv <martinesj405@gmail.com>
2025-02-18 11:28:20 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(cors({
-  origin: "http://localhost:5173/", 
+  origin: "http://localhost:5173", 
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
   methods: ["GET", "POST", "PUT", "DELETE"],
