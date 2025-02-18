68cb5838d7324e7fb4cf8fbe3a4b6a34eb335e48
luiiszv <martinesj405@gmail.com>
2025-02-18 10:36:28 -0500

    feat: api up

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(morgan("dev"));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE"],
