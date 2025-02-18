c69b31c548194500f983588548f32c745ba31df2
luiiszv <martinesj405@gmail.com>
2025-02-18 11:08:35 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -21,8 +21,9 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use(cors({
-  origin: "*",
+  origin: "http://localhost:5173",
   credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
