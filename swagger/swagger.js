1d032ff6ee9462c6b8c572d02d1f8d4a610f45b6
luiiszv <martinesj405@gmail.com>
2025-02-19 08:47:23 -0500

    apiDocs

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -23,7 +23,7 @@ const options = {
         ],
         servers: [
             {
-                url: 'http://localhost:4000',
+                url: 'https://backend-pesv.vercel.app',
                 description: 'Local server',
             },
         ],
