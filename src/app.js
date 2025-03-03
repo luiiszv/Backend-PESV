95e051c421cd2fc5136b0f76c212454105912b56
luis <martinesj405@gmail.com>
2025-03-03 10:11:21 -0500

    feat: addCors

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(morgan("dev"));
 
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://front-end-pesv.vercel.app", 
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
   methods: ["GET", "POST", "PUT", "DELETE"],
